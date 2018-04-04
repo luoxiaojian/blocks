f1641ccc41aa412b52e5658b71d5f546eecb5656
luoxiaojian <359651204@qq.com>
2018-04-05 03:03:02 +0800

    Implemented block for lkl dataset.

--- block.h
+++ block.h
@@ -1,28 +1,208 @@
 #ifndef BLOCK_H_
 #define BLOCK_H_
 
+#include <assert.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-class Block16 {
+#include <string>
+#include <vector>
+
+#define MIN(a, b) ((a) > (b) ? (b) : (a))
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+#define STD_TIME_STR_LENGTH 19
+#define FULL_TIME_STR_LENGTH 23
+
+template <int size>
+class Block {
  public:
-  char buf[16];
+  Block() {}
+  Block(char* str, int length) {
+    memset(&buf[0], 0, size);
+    memcpy(&buf[0], str, MIN(size, length));
+  }
+
+  inline void SetBuffer(char* str, int length) {
+    memset(&buf[0], 0, size);
+    memcpy(&buf[0], str, MIN(size, length));
+  }
+
+  inline char* GetBuffer() { return &buf[0]; }
+
+  inline char operator[](int loc) { return buf[loc]; }
+
+  inline Block& operator=(const Block& b) { memcpy(&buf[0], &b.buf[0], size); }
+
+  friend bool operator==(const Block& b1, const Block& b2) {
+    return (memcmp(&b1.buf[0], &b2.buf[0], size) == 0);
+  }
+
+  friend bool operator!=(const Block& b1, const Block& b2) {
+    return (memcmp(&b1.buf[0], &b2.buf[0], size) != 0);
+  }
+
+ private:
+  char buf[size];
 };
 
-class Block32 {
- public:
-  char buf[32];
+enum ApplyInfoState {
+  kAISOtherRefuse = 0,
+  kAISQRefuse = 1,
+  kAISNull = 2,
+};
+
+enum ApplyInfoState2 {
+  kAIS2Refuse = 0,
+  kAIS2Pass = 1,
 };
 
 class ApplyInfo {
  public:
-  unsigned char state;
-  unsigned char label;
-  unsigned short state2;
-  int history_days;
-  int current_days;
-  uint64_t timestamp;
-  char index[32];
-  char id_num[32];
+  ApplyInfo() {}
+
+  ApplyInfo(const ApplyInfo& rhs) { memcpy(this, &rhs, sizeof(ApplyInfo)); }
+
+  ApplyInfo& operator=(const ApplyInfo& rhs) {
+    memcpy(this, &rhs, sizeof(ApplyInfo));
+    return *this;
+  }
+
+  inline void Init(char* line) {
+    char* ptr1 = line;
+    char* ptr2 = strchr(ptr1, '\001');
+    order_id_.SetBuffer(ptr1, ptr2 - ptr1);
+    ptr1 = ptr2 + 1;
+    ptr2 = strchr(ptr1, '\001');
+    SetState(ApplyInfoState(atoi(ptr1)));
+    ptr1 = ptr2 + 1;
+    ptr2 = strchr(ptr1, '\001');
+    SetTimeStamp(ptr1);
+    ptr1 = ptr2 + 1;
+    ptr2 = strchr(ptr1, '\001');
+    SetState2(ApplyInfoState2(atoi(ptr1)));
+    ptr1 = ptr2 + 1;
+    ptr2 = strchr(ptr1, '\001');
+    SetHistoryDays(atoi(ptr1));
+    ptr1 = ptr2 + 1;
+    ptr2 = strchr(ptr1, '\001');
+    SetCurrentDays(atoi(ptr1));
+    ptr1 = ptr2 + 1;
+    ptr2 = strchr(ptr1, '\001');
+    id_.SetBuffer(ptr1, ptr2 - ptr1);
+    ptr1 = ptr2 + 1;
+    SetLabel(atoi(ptr1));
+  }
+
+  inline Block<32>& GetOrderId() { return order_id_; }
+  inline Block<32>& GetIDNumber() { return id_; }
+
+  inline ApplyInfoState GetState() {
+    int res = (state_ & 65535u);
+    return ApplyInfoState(res);
+  }
+
+  inline ApplyInfoState2 GetState2() {
+    int res = (state_ >> 16);
+    return ApplyInfoState2(res);
+  }
+
+  inline int GetLabel() { return label_; }
+
+  inline void SetState(ApplyInfoState s) {
+    unsigned x = s;
+    state_ = ((state_ & 4294901760u) | x);
+  }
+
+  inline void SetState2(ApplyInfoState2 s) {
+    unsigned x = s;
+    state_ = ((x << 16) | (state_ & 65535u));
+  }
+
+  inline void SetLabel(int l) { label_ = l; }
+
+  inline uint64_t GetTimeStamp() { return timestamp_; }
+
+  inline void SetTimeStamp(char* str_time) {
+    struct tm stm;
+    int iY, iM, iD, iH, iMin, iS, iMs;
+    memset(&stm, 0, sizeof(stm));
+    iY = atoi(str_time);
+    iM = atoi(str_time + 5);
+    iD = atoi(str_time + 8);
+    iH = atoi(str_time + 11);
+    iMin = atoi(str_time + 14);
+    iS = atoi(str_time + 17);
+    iMs = atoi(str_time + 20);
+
+    stm.tm_year = iY - 1900;
+    stm.tm_mon = iM - 1;
+    stm.tm_mday = iD;
+    stm.tm_hour = iH;
+    stm.tm_min = iMin;
+    stm.tm_sec = iS;
+
+    timestamp_ = mktime(&stm);
+    timestamp_ = timestamp_ * 1000 + iMs;
+  }
+
+  inline void GetTimeStampString(std::string& str) {
+    str.resize(FULL_TIME_STR_LENGTH + 1);
+    int iMs = timestamp_ % 1000;
+    time_t t = timestamp_ / 1000;
+    struct tm* lmt = localtime(&t);
+    strftime(&str[0], FULL_TIME_STR_LENGTH, "%Y-%m-%d %H:%M:%S", lmt);
+    sprintf(&str[STD_TIME_STR_LENGTH], ".%03d", iMs);
+  }
+
+  inline void SetHistoryDays(int hd) { hist_days_ = hd; }
+
+  inline void SetCurrentDays(int cd) { curr_days_ = cd; }
+
+  inline int GetHistoryDays() { return hist_days_; }
+
+  inline int GetCurrentDays() { return curr_days_; }
+
+ private:
+  unsigned state_;  // state_[0:15] - state, state_[16:31] - state2
+  int label_;
+  int hist_days_;
+  int curr_days_;
+  uint64_t timestamp_;
+  Block<32> order_id_;
+  Block<32> id_;
 };
+
+#define BSPACE_MIN_ITEM_NUM 128
+
+template <typename T>
+class BlockSpace {
+ public:
+  BlockSpace() : capacity_(0), cur_(0), head_(NULL) {}
+  ~BlockSpace() { Clear(); }
+  inline T* GetAnItem() {
+    if (unlikely(cur_ == capacity_)) {
+      head_ = new T[BSPACE_MIN_ITEM_NUM];
+      data_.push_back(head_);
+      capacity_ = BSPACE_MIN_ITEM_NUM;
+      cur_ = 0;
+    }
+    return &head_[cur_++];
+  }
+  void Clear() {
+    for (auto p : data_) delete[] p;
+    data_.clear();
+  }
+
+ private:
+  std::vector<T*> data_;
+  int capacity_, cur_;
+  T* head_;
+};
+
+#undef BSPACE_MIN_ITEM_NUM
+#undef unlikely
 
 #endif
--- test.cc
+++ test.cc
@@ -1,10 +1,86 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <string>
 
 #include "block.h"
 
 int main(int argc, char **argv) {
-  printf("sizeof(Block16) = %d\n", sizeof(Block16));
-  printf("sizeof(Block32) = %d\n", sizeof(Block32));
-  printf("sizeof(ApplyInfo) = %d\n", sizeof(ApplyInfo));
+  ApplyInfo ai;
+  char order_id[32];
+  char id_number[32];
+  std::string tsstr;
+  sprintf(order_id, "ABC123");
+  sprintf(id_number, "XYZ789");
+  ai.GetOrderId().SetBuffer(order_id, sizeof(order_id));
+  ai.GetIDNumber().SetBuffer(id_number, sizeof(id_number));
+  ai.SetState(kAISOtherRefuse);
+  ai.SetState2(kAIS2Refuse);
+  ai.SetTimeStamp("2017-03-29 16:42:36.851");
+  ai.SetLabel(1);
+  ai.SetHistoryDays(12);
+  ai.SetCurrentDays(3);
+
+  printf("order_id: %s\n", ai.GetOrderId().GetBuffer());
+  printf("id_number: %s\n", ai.GetIDNumber().GetBuffer());
+  printf("state: %d, (%d)\n", ai.GetState(), kAISOtherRefuse);
+  printf("state2: %d, (%d)\n", ai.GetState2(), kAIS2Refuse);
+  tsstr.clear();
+  ai.GetTimeStampString(tsstr);
+  printf("timestamp: %s\n", tsstr.c_str());
+  printf("timestamp-integer: %llu\n", ai.GetTimeStamp());
+  printf("label: %d\n", ai.GetLabel());
+  printf("HistoryDays: %d\n", ai.GetHistoryDays());
+  printf("CurrentDays: %d\n", ai.GetCurrentDays());
+  printf("=========================================\n");
+
+  ApplyInfo ai2(ai);
+
+  printf("order_id: %s\n", ai2.GetOrderId().GetBuffer());
+  printf("id_number: %s\n", ai2.GetIDNumber().GetBuffer());
+  printf("state: %d, (%d)\n", ai2.GetState(), kAISOtherRefuse);
+  printf("state2: %d, (%d)\n", ai2.GetState2(), kAIS2Refuse);
+  tsstr.clear();
+  ai2.GetTimeStampString(tsstr);
+  printf("timestamp: %s\n", tsstr.c_str());
+  printf("timestamp-integer: %llu\n", ai2.GetTimeStamp());
+  printf("label: %d\n", ai2.GetLabel());
+  printf("HistoryDays: %d\n", ai2.GetHistoryDays());
+  printf("CurrentDays: %d\n", ai2.GetCurrentDays());
+  printf("=========================================\n");
+
+  ApplyInfo ai3 = ai2;
+
+  printf("order_id: %s\n", ai3.GetOrderId().GetBuffer());
+  printf("id_number: %s\n", ai3.GetIDNumber().GetBuffer());
+  printf("state: %d, (%d)\n", ai3.GetState(), kAISOtherRefuse);
+  printf("state2: %d, (%d)\n", ai3.GetState2(), kAIS2Refuse);
+  tsstr.clear();
+  ai3.GetTimeStampString(tsstr);
+  printf("timestamp: %s\n", tsstr.c_str());
+  printf("timestamp-integer: %llu\n", ai3.GetTimeStamp());
+  printf("label: %d\n", ai3.GetLabel());
+  printf("HistoryDays: %d\n", ai3.GetHistoryDays());
+  printf("CurrentDays: %d\n", ai3.GetCurrentDays());
+  printf("=========================================\n");
+
+  ai.Init(
+      "XNW33822498346501120\0010\0012017-03-29 "
+      "16:35:39.818\0010\0010\0010\00184f867cf22c8542782ff23260ca255f3\0012");
+
+  printf("order_id: %s\n", ai.GetOrderId().GetBuffer());
+  printf("id_number: %s\n", ai.GetIDNumber().GetBuffer());
+  printf("state: %d, (%d)\n", ai.GetState(), kAISOtherRefuse);
+  printf("state2: %d, (%d)\n", ai.GetState2(), kAIS2Refuse);
+  tsstr.clear();
+  ai.GetTimeStampString(tsstr);
+  printf("timestamp: %s\n", tsstr.c_str());
+  printf("timestamp-integer: %llu\n", ai.GetTimeStamp());
+  printf("label: %d\n", ai.GetLabel());
+  printf("HistoryDays: %d\n", ai.GetHistoryDays());
+  printf("CurrentDays: %d\n", ai.GetCurrentDays());
+  printf("=========================================\n");
+
   return 0;
 }
